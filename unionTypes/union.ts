81fdfdeba4e34aed6e73561d1a99159e4fe586d2
kummari-pavan <kpavan892002@gmail.com>
2024-10-03 21:44:03 +0530

    [PAVAN]ADD: adding Unions

---
+++ unionTypes/union.ts
@@ -0,0 +1,26 @@
+// TypeScript Union Types
+// Union types are used when a value can be more than a single type.
+
+// Such as when a property would be string or number.
+
+// Union | (OR)
+// Using the | we are saying our parameter is a string or number:
+
+// ExampleGet your own TypeScript Server
+
+function printStatusCode1(code: string | number) {
+  console.log(`My status code is ${code}.`)
+}
+printStatusCode1(404);
+printStatusCode1('404');
+
+// Union Type Errors
+// Note: you need to know what your type is when union types are being used to avoid type errors:
+
+// Example
+function printStatusCode(code: string | number) {
+//   console.log(`My status code is ${code.toUpperCase()}.`) // error: Property 'toUpperCase' does not exist ontype 'string | number'.
+//   Property 'toUpperCase' does not exist on type 'number'
+}
+// In our example we are having an issue invoking toUpperCase() as its a string method and number doesn't have access to it.
+
