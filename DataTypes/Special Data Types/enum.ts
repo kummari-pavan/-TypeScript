b07d2e901ed12897fe66d4e1700fb5d81ed16561
kummari-pavan <kpavan892002@gmail.com>
2024-10-03 21:34:08 +0530

    [PAVAN]ADD: adding Aliases_and_Interfaces

--- DataTypes/Special Data Types/enum.ts
+++ DataTypes/Special Data Types/enum.ts
@@ -113,3 +113,65 @@ function getTrafficLightAction(light: TrafficLight): string {
 
 let currentLight: TrafficLight = TrafficLight.Green;
 console.log(getTrafficLightAction(currentLight));  // Output: You can go now!
+
+// Numeric Enums - Default
+// By default, enums will initialize the first value to 0 and add 1 to each additional value:
+
+// Example
+enum CardinalDirections {
+  North,
+  East,
+  South,
+  West
+}
+let currentDirection = CardinalDirections.North;
+// logs 0
+console.log(currentDirection);
+// throws error as 'North' is not a valid enum
+//currentDirection = 'North'; // Error: "North" is not assignable to type 'CardinalDirections'.
+
+// Numeric Enums - Initialized
+// You can set the value of the first numeric enum and have it auto increment from that:
+
+//Example
+enum CardinalDirections1 {
+  North = 1,
+  East,
+  South,
+  West
+}
+// logs 1
+console.log(CardinalDirections1.North);
+// logs 4
+console.log(CardinalDirections1.West);
+
+
+// Numeric Enums - Fully Initialized
+// You can assign unique number values for each enum value. Then the values will not incremented automatically:
+
+// Example
+enum StatusCodes {
+  NotFound = 404,
+  Success = 200,
+  Accepted = 202,
+  BadRequest = 400
+}
+// logs 404
+console.log(StatusCodes.NotFound);
+// logs 200
+console.log(StatusCodes.Success);
+
+// String Enums
+// Enums can also contain strings. This is more common than numeric enums, because of their readability and intent.
+
+// Example
+enum CardinalDirections2 {
+  North = 'North',
+  East = "East",
+  South = "South",
+  West = "West"
+};
+// logs "North"
+console.log(CardinalDirections2.North);
+// logs "West"
+console.log(CardinalDirections2.West);
